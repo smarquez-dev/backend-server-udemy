2226b181e6ae269219a1dfa255fe2b087496e546
Sirgaden <smarquez.dev@gmail.com>
2018-07-16 17:48:41 +0200

    Sección 11 terminada - CRUD - Imagenes

--- app.js
+++ app.js
@@ -13,9 +13,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+// * Server index config
+
 // * Importar rutas
 var appRoutes = require('./routes/app');
 var usuarioRoutes = require('./routes/usuario');
+var hospitalRoutes = require('./routes/hospital');
+var medicoRoutes = require('./routes/medico');
+var busquedaRoutes = require('./routes/busqueda');
+var uploadRoutes = require('./routes/upload');
+var imagenesRoutes = require('./routes/imagenes');
 var loginRoutes = require('./routes/login');
 
 // * Conexión a la base de datos
@@ -33,6 +40,11 @@ mongoose.connection.openUri('mongodb://@ds135061.mlab.com:35061/hospital_db', {
 
 // * Rutas
 app.use('/usuario', usuarioRoutes);
+app.use('/hospital', hospitalRoutes);
+app.use('/medico', medicoRoutes);
+app.use('/busqueda', busquedaRoutes);
+app.use('/upload', uploadRoutes);
+app.use('/imagenes', imagenesRoutes);
 app.use('/login', loginRoutes);
 app.use('/', appRoutes);
 
